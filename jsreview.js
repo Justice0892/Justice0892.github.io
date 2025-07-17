36d2a386cf7398c057a92fcae877722da5dab4ca
Justice0892 <jballa26@lab.cpcc.edu>
2025-07-17 13:16:04 -0400

    Create jsreview.js

---
+++ jsreview.js
@@ -0,0 +1,14 @@
+var stemclass = "Technology";
+let myName = "Justice";
+
+console.log(`This code was written in the STEM PREP ${stemclass} class`);
+
+alert(`This code was written in the STEM PREP ${stemclass} class by ${myName}`);
+
+var name = prompt("Hello, what is your name?");
+var school = prompt("What school do you attend? ");
+var grade = prompt("What grade are you in? ");
+
+document.getElementById("name").innerHTML = "Name: <b>" + name + "</b>";
+document.getElementById("school").innerHTML = "School: <b>" + school + "</b>";
+document.getElementById("grade").innerHTML = "Grade: <b>" + grade + "</b>";
