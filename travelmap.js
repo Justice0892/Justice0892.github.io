fac32ceb195cf64432483c5a6a733709960ff61f
Justice0892 <jballa26@lab.cpcc.edu>
2025-07-17 14:24:52 -0400

    Update travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -9,4 +9,16 @@ let map = L.map("map",{
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+}).addTo(map); 
+
+let circleMarker01 = L.circle(coordinates,{
+  color: "black",
+  fillColor: "yellow",
+  fillOpacity: 0.5,
+  radius: 500
+}).bindPopup("
+             <center
+            <h3>Hello from Paris!</h3>
+             <iframe width="560" height="315" src="https://www.youtube.com/embed/RmnRF_lNDbA?si=2Xz0tpgmtffGEMI0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+             </center>").addTo(map);
+             
