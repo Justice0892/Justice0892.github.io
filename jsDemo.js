36e1dd803c4f0c6596beed050511ea1eff30ee9e
Justice0892 <jballa26@lab.cpcc.edu>
2025-07-17 13:12:44 -0400

    Create jsDemo.js

---
+++ jsDemo.js
@@ -0,0 +1,10 @@
+console.log("This output is from the external JS file");
+var name = "Justice";
+let school = "JDS";
+var grade = "Graduate";
+console.log(`Name: ${name}`);
+console.log(`School: ${school}`);
+console.log(`Grade: ${grade}`);
+document.getElementById("name").innerHTML = "<b>Name: </b><i>" + name + "</i>";
+document.getElementById("school").innerHTML = "<b>School: </b><i>" + school + "</i>";
+document.getElementById("grade").innerHTML = "<b>Grade: </b><i>" + grade + "</i>";
